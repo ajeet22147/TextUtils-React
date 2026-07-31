b173884f7d5686babc052d6f73f7bec230c714b9
Ajeet Singh <ajeetsingh22147@gmail.com>
2026-07-31 09:25:26 +0530

    add file

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message) => {
+    setAlert({
+      msg: message,
+      type: "success",
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
+
+  return (
+    <>
+      <Router>
+        <Navbar
+          title="TextUtils"
+          about="About"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+          <Route path="/about"element={<About />}/>
+          <Route exact path="/" element={
+            <TextForm
+              showAlert={showAlert}
+              heading="Enter text to analyze below"
+              mode={mode}
+            />}/>
+          </Routes>
+        </div>
+      </Router>
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        // console.log("Uppercase was clicked:" + text);
+        setText("You have clicked on handleUpClick");
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to Uppercase!", "success");
+    }
+    const handleLoClick = () => {
+        // console.log("Lowercase was clicked:" + text);
+        setText("You have clicked on handleLoClick");
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to Lowercase!", "success");
+    }
+    const handleClearClick = () => {
+        // console.log("Clear was clicked:" + text);
+        setText("You have clicked on handleClearClick");
+        setText('');
+        props.showAlert("Text cleared!", "success");
+    }
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState('');
+
+  return (
+    <>
+    <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#042743':'white', color: props.mode==='dark'?'white':'#042743'}} id="mybox" rows="8"></textarea>
+      </div>
+      <button className="btn btn-primary mx-4" onClick={handleUpClick} >Convert to Uppercase</button>
+      <button className="btn btn-primary mx-4" onClick={handleLoClick} >Convert to Lowercase</button>
+      <button className="btn btn-primary mx-4" onClick={handleClearClick} >Text Clear</button>
+    </div>
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter something in the text area above to preview"}</p>
+    </div>
+    </>
+  );
+}
